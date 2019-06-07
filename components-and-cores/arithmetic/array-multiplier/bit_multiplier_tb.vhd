9dd4feef9a85172c18804c16c8759b10b58cf13a
Mauri Mustonen <mauri_mustonen@hotmail.com>
2019-06-07 23:30:44 +0300

    more testing

--- components-and-cores/arithmetic/array-multiplier/bit_multiplier_tb.vhd
+++ components-and-cores/arithmetic/array-multiplier/bit_multiplier_tb.vhd
@@ -6,12 +6,12 @@ use ieee.std_logic_1164.all;
 context vunit_lib.vunit_context;
 
 
-entity tb_example is
+entity tb_bit_multiplier is
     generic (runner_cfg : string);
 end entity;
 
 
-architecture tb of tb_example is
+architecture tb of tb_bit_multiplier is
     signal x_in, y_in, c_in, p_in : std_logic := '0';
     signal x_out, y_out, c_out, p_out : std_logic;
 begin
@@ -20,19 +20,42 @@ begin
     main : process
     begin
         test_runner_setup(runner, runner_cfg);
-
         while test_suite loop
-            if run("test_without_carry") then
+            if run("multiply_with_x") then
                 x_in <= '1';
-                y_in <= '0';
                 wait for 1 ps;
-                check(y_out = '0');
+
                 check(x_out = '1');
-            elsif run("second test") then
-                report "second";
+                check(y_out = '0');
+                check(p_out = '0');
+                check(c_out = '0');
+            elsif run("multiply_with_y") then
+                y_in <= '1';
+                wait for 1 ps;
+
+                check(x_out = '0');
+                check(y_out = '1');
+                check(p_out = '0');
+                check(c_out = '0');
+            elsif run("multiply_with_x_and_y") then
+                x_in <= '1';
+                y_in <= '1';
+                wait for 1 ps;
+
+                check(x_out = '1');
+                check(y_out = '1');
+                check(p_out = '1');
+                check(c_out = '1');
+            elsif run("multiply_with_partial") then
+                p_in <= '1';
+                wait for 1 ps;
+
+                check(x_out = '0');
+                check(y_out = '0');
+                check(p_out = '1');
+                check(c_out = '1');
             end if;
         end loop;
-
-        test_runner_cleanup(runner); -- Simulation ends here
+        test_runner_cleanup(runner);
     end process;
 end architecture;
