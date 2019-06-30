a6096007d37c00b2c497b4261367c1a0ca78fee6
Mauri Mustonen <mauri_mustonen@hotmail.com>
2019-06-30 23:27:02 +0300

    datapath

--- components-and-cores/arithmetic/sequential-multiplier/datapath.vhd
+++ components-and-cores/arithmetic/sequential-multiplier/datapath.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 
 entity datapath is
@@ -12,7 +13,7 @@ entity datapath is
         shift_a : in std_logic;
         msb_out : in std_logic;
         lsb_out : in std_logic;
-        sel_num : in std_logic;
+        sel_sum : in std_logic;
         data : inout std_logic_vector(7 downto 0);
         a0 : out std_logic
     );
@@ -20,7 +21,39 @@ end entity;
 
 
 architecture rtl of datapath is
-
+    signal sum, shift_add : std_logic_vector(7 downto 0);
+    signal a, b, p : std_logic_vector(7 downto 0);
+    signal co : std_logic;
+    signal op : std_logic_vector(1 downto 0);
+    signal result : std_logic_vector(8 downto 0);
 begin
+    process (clk) begin
+        if rising_edge(clk) then
+            if (load_b = '1') then
+                b <= data;
+            end if;
+        end if;
+    end process;
+
+    process (clk) begin
+        if rising_edge(clk) then
+            if (load_p = '1') then
+                p <= (co and sel_sum) & shift_add(7 downto 1);
+            end if;
+        end if;
+    end process;
+
+    process (clk) begin
+        if rising_edge(clk) then
+            case op is
+                when "01" => a <= shift_add(0) & a(7 downto 1);
+                when "10" => a <= data;
+                when others => a <= a;
+            end case;
+        end if;
+    end process;
+
+    -- How to add two std_logic_vectors together?
+    result <= unsigned('0' & p) + unsigned('0' & b);
 
 end architecture;
